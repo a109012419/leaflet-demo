e9881484985eab99cb999563b5df16dba71e5c58
tianzhenwuxie98 <109012419@qq.com>
2019-10-13 23:41:05 +0800

    提交leaflet的demo

---
+++ js/test.js
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+import { myName, myAge, myfn, myClass } from './ex.js';
+console.log(myName);
+
+
+=======
+let a=Symbol['ss'];
+console.log(typeof(a));
+>>>>>>> 80a21b6e3cc3db18bc0f577b7c080142cdeb5743
